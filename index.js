4a994771e9294c765a394e27f9649914d8e1b7aa
Stathis Stathopoulos <stathopoulos.stathis98@gmail.com>
2022-03-07 03:56:13 +0200

    Event Handler Added

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ import { config } from "dotenv";
 
 config();
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("messageCreate", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
 });
 
 client.login(process.env.TOKEN);
